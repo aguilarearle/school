aedf8ee1c40ce14a24f9fd773f6ab15fcde73183
aguilarearle <aguilarearle@users.noreply.github.com>
2017-10-16 19:29:40 -0700

    15 min check
    15 min check

--- cs145/CS145_HW1/HW1_Programming/LogisticRegression/logicsticRegression.py
+++ cs145/CS145_HW1/HW1_Programming/LogisticRegression/logicsticRegression.py
@@ -11,6 +11,7 @@ class logistic:
     #******************************************************
     def __init__(self, parameters):
         self.parameters = parameters
+        self.alpha = .001
     #******************************************************
     ########## Feel Free to Add Helper Functions ##########
     #******************************************************
@@ -34,9 +35,9 @@ class logistic:
         p_x1 = - ( e_x1 / (1 + e_x1) )
         p_x2 = - ( e_x2 / (1 + e_x2) )
         p_x3 = - ( e_x3 / (1 + e_x3) )
-        grad_1 = - P_x1 + (1 - p_x2) + (1- p_x3)
-        grad_2 = - 60*P_x1 + 64*(1 - p_x2) + 73*(1- p_x3)
-        grad_3 = - 155*P_x1 + 135*(1 - p_x2) + 170*(1- p_x3)
+        grad_1 = - p_x1 + (1 - p_x2) + (1- p_x3)
+        grad_2 = - 60 * p_x1 + 64 * (1 - p_x2) + 73 * (1- p_x3)
+        grad_3 = - 155 * p_x1 + 135 * (1 - p_x2) + 170 * (1- p_x3)
         gradients.append(grad_1)
         gradients.append(grad_2)
         gradients.append(grad_3)
@@ -44,15 +45,35 @@ class logistic:
     #******************************************************
     def iterate(self):
         ##################### Please Fill Missing Lines Here #####################
+        hessian = self.hessian()
+        gradients = self.gradients()
+        gradients = numpy.array(gradients)
+        self.parameters = self.parameters + self.alpha *  numpy.dot(numpy.linalg.inv(hessian), gradients)
         return self.parameters
     #******************************************************
     def hessian(self):
         n = len(self.parameters)
         hessian = numpy.zeros((n, n))
+
         ##################### Please Fill Missing Lines Here #####################
+        e_x1 = exp(self.parameters[0] + self.parameters[1] * 60 + self.parameters[2] * 155)
+        e_x2 = exp(self.parameters[0] + self.parameters[1] * 64 + self.parameters[2] * 135)
+        e_x3 = exp(self.parameters[0] + self.parameters[1] * 73 + self.parameters[2] * 170)
+        p_x1 = - (e_x1 / (1 + e_x1))
+        p_x2 = - (e_x2 / (1 + e_x2))
+        p_x3 = - (e_x3 / (1 + e_x3))
+
+        mat1 = numpy.matrix([[p_x1,60*p_x1,155*p_x1],
+                             [p_x2,64*p_x2,135*p_x2],
+                             [p_x3,73*p_x3,170*p_x3]])
+        mat2 = numpy.matrix([[1*(1-p_x1)  , 1*(1-p_x2)  , 1*(1-p_x3)],
+                             [60*(1-p_x1) , 64*(1-p_x2) , 73*(1-p_x3)],
+                             [155*(1-p_x1), 135*(1-p_x2), 170*(1-p_x3)]])
+
+        hessian = numpy.dot(mat2, mat1)
         return hessian
 #-------------------------------------------------------------------
-parameters = []
+#parameters = []
 ##################### Please Fill Missing Lines Here #####################
 ## initialize parameters
 parameters = [.25,.25, .25]
